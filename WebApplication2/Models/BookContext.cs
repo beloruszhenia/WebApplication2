96d89991ee45871396022703650e34bc3c3b5023
beloruszhenia <beloruszhenia@gmail.com>
2020-05-20 14:31:53 +0300

    Add project files.

---
+++ WebApplication2/Models/BookContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+
+
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Кобзар", Author = "Т Шевченко", Price = 220 });
+            db.Books.Add(new Book { Name = "Кайдашева сім'я", Author = "Н Левицький", Price = 150 });
+            db.Books.Add(new Book { Name = "П'єса для чотирьох рук", Author = "І Донич", Price = 80 });
+
+            base.Seed(db);
+        }
+
+    }
+}
